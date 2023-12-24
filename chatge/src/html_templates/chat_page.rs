c739ecb75c6403d6b68c5bb39bb0572ddf2e1836
rust-enjoyer <rust-enjoyer@gmail.com>
2023-12-24 17:11:31 +0000

    fixed input area jumping + refactoring

--- chatge/src/html_templates/chat_page.rs
+++ chatge/src/html_templates/chat_page.rs
@@ -5,12 +5,6 @@ use askama::Template;
 pub struct ChatPage {}
 
 #[derive(Template)]
-#[template(path = "clicked.html")]
-pub struct Clicked {
-    pub message: String,
-}
-
-#[derive(Template)]
 #[template(path = "chat_message.html")]
 pub struct Message {
     pub from: String,
